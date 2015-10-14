0efcb00f92308bda6620971e155ae3bf8fdc2e6a
Tyler Jensen <tyjen@microsoft.com>
2015-10-14 16:39:19 -0700

    Splitting RemoteMonitoring WebJobs into two project
    There was a 'wrapper' job that combined the tasks of the two real webjobs. Had to separate out the logic into the two webjob projects and change them to be console apps.

---
+++ EventProcessor/EventProcessor.WebJob/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.EventProcessor.WebJob.Processors;
+using Microsoft.Azure.IoT.Samples.EventProcessor.WebJob.Processors;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.EventProcessor.WebJob
+{
+    public static class Program
+    {
+        static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        static IContainer eventProcessorContainer;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                BuildContainer();
+
+                StartEventProcessorHost();
+                StartActionProcessorHost();
+                StartMessageFeedbackProcessorHost();
+
+                RunAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                cancellationTokenSource.Cancel();
+                Trace.TraceError("Webjob terminating: {0}", ex.ToString());
+            }
+        }
+
+        static void BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new EventProcessorModule());
+            eventProcessorContainer = builder.Build();
+        }
+
+        static void StartEventProcessorHost()
+        {
+            Trace.TraceInformation("Starting Event Processor");
+            var eventProcessor = eventProcessorContainer.Resolve<IDeviceEventProcessor>();
+            eventProcessor.Start(cancellationTokenSource.Token);
+        }
+
+        static void StartActionProcessorHost()
+        {
+            Trace.TraceInformation("Starting action processor");
+            var actionProcessor = eventProcessorContainer.Resolve<IActionEventProcessor>();
+            actionProcessor.Start();
+        }
+
+        static void StartMessageFeedbackProcessorHost()
+        {
+            Trace.TraceInformation("Starting command feedback processor");
+            var feedbackProcessor = eventProcessorContainer.Resolve<IMessageFeedbackProcessor>();
+            feedbackProcessor.Start();
+        }
+
+        static async Task RunAsync()
+        {
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Running");
+                try
+                {
+                    await Task.Delay(TimeSpan.FromMinutes(5), cancellationTokenSource.Token);
+                }
+                catch (TaskCanceledException) { }
+            }
+        }
+    }
+}
